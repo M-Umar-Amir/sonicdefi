baa5603304d8f35b264f7941bb9ed17f364404de
Muhammad Umar Amir <125129208+M-Umar-Amir@users.noreply.github.com>
2025-02-26 07:27:53 +0000

    Integrated Sonic and Hardhat

---
+++ hardhat.config.js
@@ -0,0 +1,21 @@
+//require("@nomicfoundation/hardhat-toolbox");
+
+///** @type import('hardhat/config').HardhatUserConfig */
+//module.exports = {
+  //solidity: "0.8.28",
+//};
+
+require("@nomicfoundation/hardhat-toolbox");
+
+// Replace this private key with your Sonic account private key
+const SONIC_PRIVATE_KEY = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+
+module.exports = {
+  solidity: "0.8.28",
+  networks: {
+    sonic: {
+      url: "https://rpc.blaze.soniclabs.com",
+      accounts: [SONIC_PRIVATE_KEY]
+    }
+  }
+};
---
+++ scripts/deploy.js
@@ -0,0 +1,14 @@
+// scripts/deploy.js
+
+async function main() {
+    // Get the contract factory
+    await console.log("Hello World");
+  }
+  
+  // Catch errors if the script fails
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
